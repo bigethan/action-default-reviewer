ea942ad88a3cde37c988881ba8476b2da6f4d28f
Ethan Schlenker <e@bigethan.com>
2019-12-19 14:04:04 -0800

    catch correct event

--- entrypoint.sh
+++ entrypoint.sh
@@ -21,7 +21,6 @@ AUTH_HEADER="Authorization: token ${GITHUB_TOKEN}"
 
 action=$(jq --raw-output .action "$GITHUB_EVENT_PATH")
 number=$(jq --raw-output .pull_request.number "$GITHUB_EVENT_PATH")
-reviewer=$(jq --raw-output .requested_reviewer.login "$GITHUB_EVENT_PATH")
 # THEDATA=$(jq --raw-output . "$GITHUB_EVENT_PATH")
 
 update_review_request() {
@@ -31,23 +30,21 @@ update_review_request() {
   echo "  -H \"Content-Type: application/json\" \\"
   echo "  -H \"${AUTH_HEADER}\" \\"
   echo "  -H \"${API_HEADER}\" \\"
-  echo "  -X $1 \\"
-  echo "  -d \"{\"assignees\":[\"${reviewer}\"]}\" \\"
-  echo "  \"https://api.github.com/repos/${GITHUB_REPOSITORY}/issues/${number}/assignees\""
+  echo "  -X POST \\"
+  echo "  -d \"{\"assignees\":[\"${DEFAULT_ASSIGNEE}\"]}\" \\"
+  echo "  \"https://api.github.com/repos/${GITHUB_REPOSITORY}/pulls/${number}/requested_reviewers\""
 
   curl -sSL \
     -H "Content-Type: application/json" \
     -H "${AUTH_HEADER}" \
     -H "${API_HEADER}" \
-    -X $1 \
-    -d "{\"assignees\":[\"${reviewer}\"]}" \
-    "https://api.github.com/repos/${GITHUB_REPOSITORY}/issues/${number}/assignees"
+    -X POST \
+    -d "{\"reviewers\":[\"${DEFAULT_ASSIGNEE}\"]}" \
+    "https://api.github.com/repos/${GITHUB_REPOSITORY}/pulls/${number}/requested_reviewers"
 }
 
-if [[ "$action" == "review_requested" ]]; then
-  update_review_request 'POST'
-elif [[ "$action" == "review_request_removed" ]]; then
-  update_review_request 'DELETE'
+if [[ "$action" == "opened" ]]; then
+  update_review_request
 else
   echo "Ignoring action ${action}"
   exit 0
